4b92a97e89995aee64c5883ab4011418ea4e0fe1
Dina Tahoun <dinatahoun992@gmail.com>
2020-10-02 02:03:49 +0200

    basic canvas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
 
 function App() {
+  const canvas = useRef(null);
+  CanvasRenderingContext2D.prototype.wrapText = function (
+    text,
+    x,
+    y,
+    maxWidth,
+    lineHeight
+  ) {
+    var lines = text.split("\n");
+
+    for (var i = 0; i < lines.length; i++) {
+      var words = lines[i].split(" ");
+      var line = "";
+
+      for (var n = 0; n < words.length; n++) {
+        var testLine = line + words[n] + " ";
+        var metrics = this.measureText(testLine);
+        var testWidth = metrics.width;
+        if (testWidth > maxWidth && n > 0) {
+          this.fillText(line, x, y);
+          line = words[n] + " ";
+          y += lineHeight;
+        } else {
+          line = testLine;
+        }
+      }
+
+      this.fillText(line, x, y);
+      y += lineHeight;
+    }
+  };
+  useEffect(() => {
+    const ctx = canvas.current.getContext("2d");
+    window.onload = () => {
+      let img = new Image();
+      img.crossOrigin = "anonymous";
+      img.src =
+        "https://img.freepik.com/free-photo/jasmine-flower-greenery_34266-955.jpg?size=626&ext=jpg";
+      ctx.drawImage(img, 0, 0, 600, 600);
+      ctx.font = "40px Courier";
+      ctx.textAlign = "start";
+      ctx.wrapText("Hello World! adasd vdsfahdsah sgdua", 60, 20, 640, 30);
+    };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas ref={canvas} width={640} height={425} />
+      {/* <img
+        ref={image}
+        src="https://img.freepik.com/free-photo/jasmine-flower-greenery_34266-955.jpg?size=626&ext=jpg"
+      /> */}
+      <img src={canvas.current ? canvas.current.toDataURL() : ""} />
     </div>
   );
 }
