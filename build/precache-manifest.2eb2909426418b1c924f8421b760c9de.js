07eec0465a1c59dd823cbcbabfe5980a277f77f3
Dina Tahoun <dinatahoun992@gmail.com>
2020-10-05 00:04:01 +0200

    build

---
+++ build/precache-manifest.2eb2909426418b1c924f8421b760c9de.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d9bf25aa120e9eea14625a81a0e43f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d8650c2d6f83cd690e2",
+    "url": "/static/css/main.c520ab04.chunk.css"
+  },
+  {
+    "revision": "9d4d9680b1c3d2fdd61e",
+    "url": "/static/js/2.baf68c90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.baf68c90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d8650c2d6f83cd690e2",
+    "url": "/static/js/main.45c1c3f4.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "599ca5ba7614a9585e183589848209f8",
+    "url": "/static/media/0.599ca5ba.jpeg"
+  },
+  {
+    "revision": "ad0b366124bc567eb1ffd6b09ce3448d",
+    "url": "/static/media/1.ad0b3661.jpg"
+  },
+  {
+    "revision": "10c8c7132da691b6904caafa634a2405",
+    "url": "/static/media/2.10c8c713.jpg"
+  },
+  {
+    "revision": "08880df2c48496e7cbfbd91e55dfea74",
+    "url": "/static/media/3.08880df2.jpg"
+  },
+  {
+    "revision": "7e39ab4a3f7f1d7357b098e5eca1e982",
+    "url": "/static/media/4.7e39ab4a.jpg"
+  },
+  {
+    "revision": "2bb3febcae0e12e5020dd2e391d1c952",
+    "url": "/static/media/5.2bb3febc.jpeg"
+  },
+  {
+    "revision": "64205a99281a9174f573f91bf5e1082e",
+    "url": "/static/media/6.64205a99.webp"
+  }
+]);
