75840c418f051086ba6f8ac6985a8c7f546ed690
Dina Tahoun <dinatahoun992@gmail.com>
2020-10-25 05:18:03 +0200

    Updates

---
+++ precache-manifest.c0e9d680c4ca29c00f927eec4dc9b96c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f423257e631a1e221749abc6f454e488",
+    "url": "/celebration-cards/index.html"
+  },
+  {
+    "revision": "ed76cda48c62bfacd797",
+    "url": "/celebration-cards/static/css/main.2179db33.chunk.css"
+  },
+  {
+    "revision": "733caf3e1a8de686c876",
+    "url": "/celebration-cards/static/js/2.214e9523.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/celebration-cards/static/js/2.214e9523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed76cda48c62bfacd797",
+    "url": "/celebration-cards/static/js/main.40c0e4e8.chunk.js"
+  },
+  {
+    "revision": "88d15b6afb90e6ead154",
+    "url": "/celebration-cards/static/js/runtime-main.d560c6cf.js"
+  },
+  {
+    "revision": "599ca5ba7614a9585e183589848209f8",
+    "url": "/celebration-cards/static/media/0.599ca5ba.jpeg"
+  },
+  {
+    "revision": "ad0b366124bc567eb1ffd6b09ce3448d",
+    "url": "/celebration-cards/static/media/1.ad0b3661.jpg"
+  },
+  {
+    "revision": "10c8c7132da691b6904caafa634a2405",
+    "url": "/celebration-cards/static/media/2.10c8c713.jpg"
+  },
+  {
+    "revision": "08880df2c48496e7cbfbd91e55dfea74",
+    "url": "/celebration-cards/static/media/3.08880df2.jpg"
+  },
+  {
+    "revision": "2bb3febcae0e12e5020dd2e391d1c952",
+    "url": "/celebration-cards/static/media/5.2bb3febc.jpeg"
+  },
+  {
+    "revision": "0e88b11a80ee2056ec07392faaf52a23",
+    "url": "/celebration-cards/static/media/6.0e88b11a.webp"
+  },
+  {
+    "revision": "d1f4d5de9fb23cb1233dfbee08eb620e",
+    "url": "/celebration-cards/static/media/download.d1f4d5de.png"
+  }
+]);
